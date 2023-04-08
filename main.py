11690bb8219a44a7879d6cbb19d1179180cb61c1
Andrii Maichub <113465119+Maichubist@users.noreply.github.com>
2023-04-08 22:34:54 +0300

    change api

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ TOKEN = os.environ['TOKEN']
 
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
-YOUTUBE_API_KEY = os.environ['TOKEN']
+YOUTUBE_API_KEY = os.environ['YOUTUBE_API_KEY']
 youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=YOUTUBE_API_KEY)
 
 bot = Bot(token=TOKEN)
