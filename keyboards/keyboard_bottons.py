51e620611ffb08aba0213c21cd4fba10f4fa7897
Andrii Maichub <113465119+Maichubist@users.noreply.github.com>
2023-04-09 13:46:35 +0300

    big bug fix

--- keyboards/keyboard_bottons.py
+++ keyboards/keyboard_bottons.py
@@ -1,20 +1,16 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-# from aiogram.utils.emoji import emojize
 
-youtube_k = KeyboardButton('YouTube')
-tiktok_k = KeyboardButton('TikTok (DON`T USE)')
-instagram_k = KeyboardButton('Instagram')
-internet_k = KeyboardButton('Internet (DON`T USE)')
+audio = KeyboardButton('Audio')
+video = KeyboardButton('Video')
 
 
 markup_keyboard_source = ReplyKeyboardMarkup(
     resize_keyboard=True, 
     one_time_keyboard=True
     ).row(
-        youtube_k)
-    #     ).add(
-    # internet_k
-    # )
+    audio,
+    video,
+)
 
 markup_yt = ReplyKeyboardMarkup(
     resize_keyboard=True, 
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import time
 import asyncio
 import os
-from shutil import rmtree
 
 from aiogram import Bot, Dispatcher, types
 from aiogram.dispatcher import FSMContext
@@ -11,15 +10,15 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 from logger import logger
-import keyboards.inline_keyboards as ik
-import keyboards.keyboard_bottons as bk
-from get_link import GetLink, GetResponse
+import keyboards.keyboard_bottons as kb
+from get_link import GetResponse
 from downloaders.youtube_downloader import send_video, send_audio
 from templates.helper import m
 from templates.greeting import g
 from validator import validate_url
 
 TOKEN = os.environ['TOKEN']
+
 
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
@@ -30,7 +29,7 @@ bot = Bot(token=TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
-content_type = ''
+user_data = {}
 
 
 @dp.message_handler(commands=["start"])
@@ -68,71 +67,48 @@ async def cmd_start(message: types.Message):
 @dp.message_handler(commands=["download"])
 async def cmd_botton(message: types.Message):
     logger.info(f"{message.from_user.id}|{message.from_user.full_name} choose {message.text}")
-    await message.reply("*To start working press the botton*", reply_markup=bk.markup_keyboard_source,
-                        parse_mode="Markdown")
+    await message.reply("Paste a link to download or use @s_yt_bot inline mode to find video in YouTube")
 
 
-@dp.message_handler(lambda message: message.text in ['YouTube', 'Instagram', 'TikTok', 'Internet'])
-async def filter_sources(message: types.Message):
-    usr_choise = message.text
-    await message.reply(text=f'What do you want to download from *{usr_choise}*',
-                        reply_markup=types.ReplyKeyboardRemove(), parse_mode="Markdown")
-    logger.info(f"{message.from_user.id}|{message.from_user.full_name} choose {usr_choise}")
-    if usr_choise == "YouTube":
-        await message.answer("Choose one option", reply_markup=ik.markup_yt)
+@dp.message_handler(lambda message: validate_url(message.text))
+async def link_processor(message: types.Message):
+    user_data[message.from_user.id] = {"link": message.text}
+    await message.answer("Choose one option", reply_markup=kb.markup_keyboard_source)
+
+
+@dp.message_handler(lambda message: message.text in ["Audio", "Video"])
+async def link_processor(message: types.Message):
+    global user_data
+    content = message.text
+    logger.info(
+        f"{message.from_user.id}|{message.from_user.full_name} choose {content}")
+    if content == "Audio":
+        try:
+            logger.info(f"{message.from_user.id}|{message.from_user.full_name}| THE PROCES BEGINS")
+            await bot.send_message(chat_id=message.from_user.id, text="It may take some time.")
+            audio_name = send_audio(user_data[message.from_user.id]["link"])
+            with open(f'{audio_name}.mp3', 'rb') as audio:
+
+                await bot.send_audio(chat_id=message.from_user.id, audio=audio)
+                logger.info(f"{message.from_user.id}|{message.from_user.full_name}| THE PROCES ENDS")
+                os.remove(f"{audio_name}.mp3")
+        except Exception as er:
+            await message.reply(f"There was an error: \n{er}")
+            logger.error(f"There was an error: {er}")
+    elif content == "Video":
+        try:
+            logger.info(f"{message.from_user.id}|{message.from_user.full_name}| THE PROCES BEGINS")
+            video = send_video(user_data[message.from_user.id]["link"])
+            await bot.send_video(chat_id=message.from_user.id, video=video)
+            logger.info(f"{message.from_user.id}|{message.from_user.full_name}| THE PROCES ENDS")
+        except Exception as er:
+            await message.from_user.id.reply(f"There was an error: \n{er}")
+            logger.error(f"There was an error:\n {er}")
     else:
-        logger.error(f"Unvalid user response {usr_choise}")
-        await bot.send_message(chat_id=message.chat.id, text=f"Something went wrong({usr_choise})")
-
-
-@dp.callback_query_handler(lambda c: c.data and c.data.startswith('yt_'))
-async def youtube_processer(callback_query: types.CallbackQuery):
-    global content_type
-    content_type = callback_query.data.split('_')[1]
-    logger.info(f"{callback_query.from_user.id}|{callback_query.from_user.full_name} choose {content_type}")
-    await bot.answer_callback_query(callback_query.id)
-    await bot.send_message(callback_query.from_user.id, f'*Paste a link or search video using @s_yt_bot inline mode:*', parse_mode="Markdown")
-    await GetLink.waiting_for_link.set()
-
-    @dp.message_handler(lambda message: message.from_user.id == callback_query.from_user.id,
-                        state=GetLink.waiting_for_link)
-    async def process_link(message: types.Message, state: FSMContext, ):
-        await bot.send_message(chat_id=message.chat.id, text="Got a link, processing it")
-        logger.info(f"{message.from_user.id}|{message.from_user.full_name} sent a link")
-        link = message.text
-        if message.text != "/exit" and validate_url(link):
-            await state.finish()
-            if content_type == "audio":
-                try:
-                    logger.info(f"{message.from_user.id}|{message.from_user.full_name}| THE PROCES BEGINS")
-                    await bot.send_message(chat_id=message.chat.id, text="It may take some time.")
-                    audio_name = send_audio(link)
-                    with open(f'{audio_name}.mp3', 'rb') as audio:
-
-                        await bot.send_audio(chat_id=message.chat.id, audio=audio)
-                        logger.info(f"{message.from_user.id}|{message.from_user.full_name}| THE PROCES ENDS")
-                except Exception as er:
-                    await message.reply(f"There was an error: \n{er}")
-                    logger.error(f"There was an error: {er}")
-            elif content_type == "video":
-                try:
-                    logger.info(f"{message.from_user.id}|{message.from_user.full_name}| THE PROCES BEGINS")
-                    video = send_video(link)
-                    await bot.send_video(chat_id=message.chat.id, video=video)
-                    logger.info(f"{message.from_user.id}|{message.from_user.full_name}| THE PROCES ENDS")
-                except Exception as er:
-                    await message.reply(f"There was an error: \n{er}")
-                    logger.error(f"There was an error:\n {er}")
-            else:
-                await bot.send_message(callback_query.from_user.id,
-                                       f'{callback_query.from_user.full_name} = {content_type}|FAILED ')
-        elif message.text == "/exit":
-            await state.finish()
-            await message.reply(f"Now you can choose another option")
-        else:
-            logger.error(f"{message.from_user.full_name} did not sent correct link")
-            await message.reply(
-                f"It doesn`t seem like a correct link. Try one more time\nIf you want to change option, press /exit")
+        await bot.send_message(message.from_user.id,
+                               f'{message.from_user.full_name} = {user_data}|FAILED ')
+    logger.info(f"User {user_data} deleted")
+    del user_data
 
 
 @dp.inline_handler()
